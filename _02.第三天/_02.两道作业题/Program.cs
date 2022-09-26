fbf650a044308d49852823e9561c614d22951ea5
linnc <nash0207@163.com>
2022-09-26 23:35:36 +0800

    update

--- _02.第三天/_02.两道作业题/Program.cs
+++ _02.第三天/_02.两道作业题/Program.cs
@@ -10,6 +10,38 @@ namespace _02.两道作业题
     {
         static void Main(string[] args)
         {
+
+            //练习，计算46天是几周零几天
+            //int days = 46;
+            //int weeks = days / 7;
+            //int day = days % 7;
+            //Console.WriteLine("{0}天是{1}周零{2}天",days,weeks,day);
+            //Console.ReadLine();
+
+
+
+            //107653秒是几天几小时几分钟几秒
+
+            //60 * 60   3600 * 24 = 86400
+
+            int time = 107653;
+            int day_num = time / 86400;
+            int hours = time % 86400 / 3600;
+            int minute = (time % 86400 % 3600) / 60;
+            int second = time % 86400 % 3600 % 60;
+            Console.WriteLine("{0}秒是{1}天{2}小时{3}分钟{4}秒", time, day_num, hours, minute, second);
+            Console.ReadLine();
+
+
+            //int seconds = 107653;
+            //int days = seconds / 86400;
+            //int secs = seconds % 86400;
+            //int hours = secs / 3600;
+            //secs = secs % 3600;
+            //int mins = secs / 60;
+            //secs = secs % 60;
+            //Console.WriteLine("{0}秒是{1}天{2}小时{3}分钟{4}秒", seconds, days, hours, mins, secs);
+            //Console.ReadLine();  //107653秒是1天5小时54分钟13秒
         }
     }
 }
---
+++ _02.第三天/_03.类型转换/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03.类型转换
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //显示类型转换、自动类型转换
+            //int -> double double -> int
+
+            //类型如果相兼容的两个变量，可以使用自动类型转换或者强制类型转换
+            //但是，如果两个类型的变量不兼容，比如string与int或者string与double
+            //这个时候我们可以使用一个叫Convert的转换工厂进行转换
+            //注意，使用Convert进行类型转换，也需要满足一个条件：
+            //面儿上必须要过的去
+
+            
+
+
+            string s = "123";
+            //将字符串转换成int或者double类型
+            double d = Convert.ToDouble(s);
+            int i = Convert.ToInt32(s);
+
+            Console.WriteLine(d);
+            Console.ReadLine();
+
+
+
+            //让用户输入姓名 语文 数学 英语 三门课的成绩
+            //然后给用户显示： XX，你的总成绩为XX分，平均成绩为XX分
+            Console.WriteLine("请输入你的姓名");
+            string name = Console.ReadLine();
+            Console.WriteLine("请输入你的语文成绩");
+            string strChinese = Console.ReadLine();
+            Console.WriteLine("请输入你的数学成绩");
+            string strMath = Console.ReadLine();
+            Console.WriteLine("请输入你的英语成绩");
+            string strEnglish = Console.ReadLine();
+
+
+            //由于字符串去相加的话，最终会变成相连接，如果要拿字符串类型的变量参与计算
+            //需要将字符串转换成int或者double类型
+
+            //int chinese = Convert.ToInt32(strChinese);
+            //int math = Convert.ToInt32(strMath);
+            //int english = Convert.ToInt32(strEnglish);
+
+            double chinese = Convert.ToDouble(strChinese);
+            double math = Convert.ToDouble(strMath);
+            double english = Convert.ToDouble(strEnglish);
+
+
+            Console.WriteLine("{0}，你的总成绩为{1}分，平均成绩为{2}分",name,chinese+math+english,(chinese + math + english)/3);
+            Console.ReadKey();
+        }
+    }
+}
